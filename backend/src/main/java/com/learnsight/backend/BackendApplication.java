5d8286bbdde0513038610dd940f212bcb8525091
letrungnguyen104 <letrungnguyen1004@gmail.com>
2026-02-06 11:26:16 +0700

    Initial commit: Monorepo structure setup

---
+++ backend/src/main/java/com/learnsight/backend/BackendApplication.java
@@ -0,0 +1,22 @@
+package com.learnsight.backend;
+
+import org.springframework.boot.SpringApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv
+				.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
